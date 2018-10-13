d884826a6d556edff57f30c2a96dff25580d9356
Yuzuki-S <yuzuki.sadoh@gmail.com>
2018-10-13 17:43:21 +1300

    video added

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -4,7 +4,6 @@ import { connect } from "react-redux";
 import Navbar from './Navbar'
 import Login from "./Login";
 import Register from "./Register";
-import Nav from "./Nav";
 import Meeting from "./Meeting";
 import History from "./History";
 import Help from "./Help";
@@ -12,25 +11,13 @@ import Help from "./Help";
 const App = ({ auth }) => (
   <Router>
     <div id='app'>
-      <section id="App_video" class="hero is-fullheight video">
-        <div class="hero-video">
+      <div id="App_video">
           <video id="bgvid" playsInline autoPlay muted loop>
             <source src="./video.mp4" type="video/mp4" />
           </video>
-        </div>
-      </section>
-      <Route path='/' component={Navbar} />
-      <div id="App_Hero" className="hero is-small is-primary">
-        <div className="hero-body has-text-centered">
-          <Link to="/" className="">
-            <h1 id="App_title" className="title is-1">
-              Dress Me Up
-            </h1>
-          </Link>
-          <Nav />
-       
-        </div>
       </div>
+      <Route path='/' component={Navbar} />
+
 
       <div className="">
         {/* {!auth.isAuthenticated && <Route exact path="/" component={Login} />} */}
